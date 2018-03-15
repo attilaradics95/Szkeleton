3f7def94c212ae7ec5dc55f3abe87ed8e4e88d13
Radics Attila <attilaradics95@gmail.com>
2018-03-15 21:42:48 +0100

    Direction -> Directions Visitor és Box osztály elkészítése

---
+++ src/Box.java
@@ -0,0 +1,33 @@
+public class Box extends Visitor{
+    public void pushTo(Tile next, Directions d){
+        system.out.println("pushTo(" + next + "," + d + ")");
+    }
+    public void pushTo(Switch next, Directions d){
+        system.out.println("pushTo(" + next + "," + d + ")");
+    }
+    public void pushTo(Hole next, Directions d){
+        system.out.println("pushTo(" + next + "," + d + ")");
+    }
+    public void pushTo(Trap next, Directions d){
+        system.out.println("pushTo(" + next + "," + d + ")");
+    }
+    public void pushTo(Target next, Directions d){
+        system.out.println("pushTo(" + next + "," + d + ")");
+    }
+    public void pushTo(Obstacle next, Directions d){
+        system.out.println("pushTo(" + next + "," + d + ")");
+    }
+    public void setUnmovable(){
+        system.out.println("setUnmovable()");
+    }
+    public void checkMovable(){
+        system.out.println("checkMovable()");
+    }
+    public void die(){
+        system.out.println("die()");
+    }
+    public void move(Directions d){
+        system.out.println("move(" + d + ")");
+    }
+
+}
---
+++ src/Visitor.java
@@ -0,0 +1,26 @@
+public abstract class Visitor {
+    public void pushTo(Tile next, Directions d){
+        system.out.println("pushTo(" + next + "," + d  + ")");
+    }
+    public void pushTo(Switch next, Directions d){
+        system.out.println("pushTo(" + next + "," + d  + ")");
+    }
+    public void pushTo(Hole next, Directions d){
+        system.out.println("pushTo(" + next + "," + d  + ")");
+    }
+    public void pushTo(Trap next, Directions d){
+        system.out.println("pushTo(" + next + "," + d  + ")");
+    }
+    public void pushTo(Target next, Directions d){
+        system.out.println("pushTo(" + next + "," + d  + ")");
+    }
+    public void pushTo(Obstacle next, Directions d){
+        system.out.println("pushTo(" + next + "," + d  + ")");
+    }
+    public void die(){
+        system.out.println("die()");
+    }
+    public void move(Directions d){
+        system.out.println("move(" + d + ")");
+    }
+}
