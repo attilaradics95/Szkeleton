7982f078e4e94350109aad328a31a1166383f5c8
Radics Attila <attilaradics95@gmail.com>
2018-03-15 20:59:17 +0100

    Switch osztály elkészítése

--- src/Switch.java
+++ src/Switch.java
@@ -1,5 +1,9 @@
-public class Column {
+public class Switch {
     public void accept(Visitor v, Direction d) {
         system.out.println("accept(" + v + "," + d + ")");
     }
+    public void switch(Box b){
+        system.out.println("switch(" + b + ")");
+    }
 }
+
--- src/Wall.java
+++ src/Wall.java
@@ -1,4 +1,5 @@
 public class Wall extends ATile {
     public void accept(Visitor v, Direction d) {
         system.out.println("accept(" + v + "," + d + ")");
+    }
 }
