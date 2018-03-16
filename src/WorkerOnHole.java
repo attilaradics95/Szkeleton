4c3a60c0f1263ce81be8c19e98847bf65d32a3ea
Sándor Bálint <balint245@gmail.com>
2018-03-16 15:30:57 +0100

    a

---
+++ src/WorkerOnHole.java
@@ -0,0 +1,11 @@
+public class WorkerOnHole {
+    public static void main(String[] args) {
+        Worker worker1 = new Worker();
+        Hole next = new Hole();
+        Tile current = new Tile();
+        next.accept(worker1, Directions.EAST);
+        worker1.pushTo(next, Directions.EAST);
+        current.setVisitor(null);
+        worker1.die();
+    }
+}
