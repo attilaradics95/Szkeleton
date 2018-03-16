c234f20f5ed39c5216623787ce388f642379c449
olcsa <oliwasi@gmail.com>
2018-03-16 16:05:44 +0100

    Metodus fixek

--- src/Visitor.java
+++ src/Visitor.java
@@ -2,35 +2,12 @@ public abstract class Visitor {
     protected ATile currentTile;
     protected boolean movable;
 
-    public void pushTo(Tile next, Directions d) {
-        System.out.println("pushTo(" + next + "," + d + ")");
-    }
-
-    public void pushTo(Switch next, Directions d) {
-        System.out.println("pushTo(" + next + "," + d + ")");
-    }
-
-    public void pushTo(Hole next, Directions d) {
-        System.out.println("pushTo(" + next + "," + d + ")");
-    }
-
-    public void pushTo(Trap next, Directions d) {
-        System.out.println("pushTo(" + next + "," + d + ")");
-    }
-
-    public void pushTo(Target next, Directions d) {
-        System.out.println("pushTo(" + next + "," + d + ")");
-    }
-
-    public void pushTo(Wall next, Directions d) {
-        System.out.println("pushTo(" + next + "," + d + ")");
-    }
-
-    public void pushTo(Column next, Directions d) {
-        System.out.println("pushTo(" + next + "," + d + ")");
-    }
-
-    public void die() {
-        System.out.println("die()");
-    }
+    abstract public void pushTo(Tile next, Directions d);
+    abstract public void pushTo(Switch next, Directions d);
+    abstract public void pushTo(Hole next, Directions d);
+    abstract public void pushTo(Trap next, Directions d);
+    abstract public void pushTo(Target next, Directions d);
+    abstract public void pushTo(Wall next, Directions d);
+    abstract public void pushTo(Column next, Directions d);
+    abstract public void die();
 }
