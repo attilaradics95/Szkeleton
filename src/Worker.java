55ccf7a3dfa7db204f8f75059ae957d3988bee43
olcsa <oliwasi@gmail.com>
2018-03-16 15:45:21 +0100

    move metodus megirva

--- src/Worker.java
+++ src/Worker.java
@@ -1,3 +1,41 @@
 public class Worker extends Visitor{
+    private Controller controller = null;
 
+    public Worker(){
+        controller = Controller.getInstance();
+    }
+
+    @Override
+    public void move(Directions d) {
+        ATile next = currentTile.getNeighbor(d);
+        next.accept(this, d);
+    }
+
+    public void pushTo(Tile next, Directions d) {
+        System.out.println("pushTo(" + next + "," + d + ")");
+    }
+
+    public void pushTo(Switch next, Directions d) {
+        System.out.println("pushTo(" + next + "," + d + ")");
+    }
+
+    public void pushTo(Hole next, Directions d) {
+        System.out.println("pushTo(" + next + "," + d + ")");
+    }
+
+    public void pushTo(Trap next, Directions d) {
+        System.out.println("pushTo(" + next + "," + d + ")");
+    }
+
+    public void pushTo(Target next, Directions d) {
+        System.out.println("pushTo(" + next + "," + d + ")");
+    }
+
+    public void pushTo(Wall next, Directions d) {
+        System.out.println("pushTo(" + next + "," + d + ")");
+    }
+
+    public void pushTo(Column next, Directions d) {
+        System.out.println("pushTo(" + next + "," + d + ")");
+    }
 }
