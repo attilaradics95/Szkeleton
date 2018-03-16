f1707694bb826a9ead80fec055b1eebc27f891cd
olcsa <oliwasi@gmail.com>
2018-03-16 13:47:16 +0100

    Game, Controller modositas, Worker

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 public class Controller {
     /**Singleton metódusok és attributumok*/
     private static Controller instance = null;
-    private Controller(){}
+    private Controller(){
+        workers = new ArrayList<>();
+        game = Game.getInstance();
+    }
     public static Controller getInstance() {
         if(instance == null) {
             instance = new Controller();
@@ -9,5 +14,18 @@ public class Controller {
         return instance;
     }
 
+    /**Attribútumok*/
+    private Game game = null;
+    private ArrayList<Worker> workers;
+    private Worker selectedworker = null;
 
+    /**Metódusok*/
+    public void selectWorker(int i){
+        System.out.println("selectWorker");
+        //TODO
+    }
+    public void moveWorker(Directions direction){
+        System.out.println("selectWorker");
+        //TODO
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,17 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Game {
     /**Singleton metódusok és attributumok*/
     private static Game instance = null;
-    private Game(){}
+    private Game(){
+        controller = Controller.getInstance();
+    }
     public static Game getInstance() {
         if(instance == null) {
             instance = new Game();
@@ -9,20 +19,100 @@ public class Game {
         return instance;
     }
 
+    /**Attributumok*/
+    Controller controller = null;
+    boolean roundover = false;
+
+
+
+    /**Privat Metódusok*/
     private void loadMap(){}
 
-    private static void startRound(){
+    private void startRound(){
         System.out.println("startRound()");
-        while(true){
+        //MAP betöltese
+        this.loadMap();
 
+        //Loop az inputok kezelesere
+        while(!roundover){
+            //Menu kiirasa
+            System.out.println("1.Irány megadása(W,A,S,D)");
+            System.out.println("2.Raktáros kiválasztása(0,1,2...99)");
+            System.out.println("3.Feladás(igiveup)");
+            System.out.println("Kérem adja meg az inputot:");
+            //Input beolvasása a konzolról
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = "";
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Regularis kifejezesek az input csekkolasara
+            Pattern selectPattern = Pattern.compile("^([0-9]|[1-9][0-9])$");
+            Pattern movePattern = Pattern.compile("^(W|A|S|D|w|a|s|d)$");
+            Matcher selectMatch = selectPattern.matcher(input);
+            Matcher moveMatch = movePattern.matcher(input);
+
+            if(selectMatch.matches()){
+                controller.selectWorker(Integer.parseInt(input));
+            }
+            if(moveMatch.matches()){
+                switch (input){
+                    case "W":
+                    case "w":
+                        controller.moveWorker(Directions.NORTH);
+                        break;
+                    case "D":
+                    case "d":
+                        controller.moveWorker(Directions.EAST);
+                        break;
+                    case "S":
+                    case "s":
+                        controller.moveWorker(Directions.SOUTH);
+                        break;
+                    case "A":
+                    case "a":
+                        controller.moveWorker(Directions.WEST);
+                        break;
+                }
+            }
+            if (input.equals("igiveup")){
+                this.endRound();
+            }
+            else{
+                System.out.println("Az input nem valid!");
+            }
         }
     }
 
     public void endRound(){
         System.out.println("endRound()");
+        //Akkor lep ki a loopbol ha megkapja a megfelelo inputot
+        while (true) {
+            System.out.println("Vége van a körnek? (Y/N)");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = "";
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (input.equals("Y") || input.equals("y")) {
+                roundover = true;
+                return;
+            }
+            if (input.equals("N") || input.equals("n")) {
+                roundover = false;
+                return;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        startRound();
+        Game game = getInstance();
+
+        game.startRound();
     }
 }
