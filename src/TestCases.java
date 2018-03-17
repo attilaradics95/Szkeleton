e1c0c5b5c4744b83a16669b79cf15ba0cf7c3356
Sándor Bálint <balint245@gmail.com>
2018-03-17 17:42:50 +0100

    MLL, MLMMF javítása

--- src/TestCases.java
+++ src/TestCases.java
@@ -41,57 +41,41 @@ public class TestCases {
     public void MLMMF() {
         Directions dir = Directions.EAST;
 
+        Controller controller = Controller.getInstance();
+
+        //Elemek létrehozása
         Worker visitor1 = new Worker();
         Box visitor2 = new Box();
         Worker visitor3 = new Worker();
         Worker visitor4 = new Worker();
-        /*Visitor visitor2 = null;
-        Visitor visitor3 = null;
-        Visitor visitor4 = null;*/
-
         Tile tile1 = new Tile();
         Tile tile2 = new Tile();
         Tile tile3 = new Tile();
         Tile tile4 = new Tile();
         Wall wall = new Wall();
-        /*ATile tile3 = null;
-        ATile tile4 = null;
-        ATile wall = null;*/
 
-        tile1.setNeighbor(tile2, dir);
-        tile2.setNeighbor(tile3, dir);
-        tile3.setNeighbor(tile4, dir);
-        tile4.setNeighbor(wall, dir);
-
+        //beállítjuk a visitorokat a mezőkre
         tile1.setVisitor(visitor1);
         tile2.setVisitor(visitor2);
         tile3.setVisitor(visitor3);
         tile4.setVisitor(visitor4);
 
+        //beállítjuk a mezőket a visitorokra
+        visitor1.setCurrentTile(tile1);
+        visitor2.setCurrentTile(tile2);
+        visitor3.setCurrentTile(tile3);
+        visitor4.setCurrentTile(tile4);
 
-        Controller controller = null;
+        //pálya beállítása
+        tile1.setNeighbors(null, tile2, null, null);
+        tile2.setNeighbors(null, tile3, null, tile1);
+        tile3.setNeighbors(null, tile4, null, tile2);
+        tile4.setNeighbors(null, wall, null, tile3);
+        wall.setNeighbors(null, null, null, tile4);
 
+        //Első munkás mozgatása
+        controller.addWorker(visitor1);
         controller.moveWorker(dir);
-        visitor1.move(dir);
-        tile2.accept(visitor1, dir);
-        visitor1.pushTo(tile2, dir);
-
-        tile3.accept(visitor2, dir);
-        visitor2.pushTo(tile3, dir);
-
-        tile4.accept(visitor3, dir);
-        visitor3.pushTo(tile4, dir);
-
-        wall.accept(visitor4, dir);
-        visitor4.pushTo(wall, dir);
-
-        visitor4.die();
-        controller.eliminateWorker(visitor4);
-        tile4.setVisitor(null);
-        tile4.setVisitor(visitor3);
-        tile3.setVisitor(visitor2);
-        tile2.setVisitor(visitor1);
-        tile1.setVisitor(null);
     }
 
     public void WorkerOnColumn() {
