24d3a81263df698dd10fe2da5fc43f94ecd4388a
SKlara <33899253+SKlara@users.noreply.github.com>
2018-03-18 20:32:47 +0100

    kommentek az IIT boldogságáért

--- src/Tabulate.java
+++ src/Tabulate.java
@@ -1,12 +1,17 @@
 public class Tabulate {
+    //egy osztály arra, hogy a függvényhívás alapján megfelelően legyenek behúzva a dolgok
 
     private static int tabNum = 0;
 
+    //növeli a tabulátorok számát
+    //minden kiírandó függvénynél meghívjuk a legelején
     public void in() {
         tabNum++;
         tabulate();
     }
 
+    //csökkenti a tabulátorok számát
+    //minden kiírandó függvénynél meghívjuk a legvégén
     public void out() {
         //System.out.println("Fuggveny visszatert.");
         tabNum--;
@@ -14,6 +19,7 @@ public class Tabulate {
 
     }
 
+    //a függvény neve előtt kiírja a megfelelő számú tabulátort
     public void tabulate() {
         for(int i = 0; i < tabNum; i++) {
             System.out.print("\t");
