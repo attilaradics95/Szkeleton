c1edb1b29340f8e9420f25cf5cb39f10dff56d4e
olcsa <oliwasi@gmail.com>
2018-04-20 23:11:30 +0200

    Trap and switch fix

--- src/Switch.java
+++ src/Switch.java
@@ -5,7 +5,6 @@ public class Switch extends ATile {
     static int instanceCounter = 0;
     int counter = 0;
    //dupla tabulátorozás ellen kikommenteltem @author Rozi
-    //Tabulate tabulate = new Tabulate();
 
     public Switch() {
         instanceCounter++;
@@ -14,13 +13,9 @@ public class Switch extends ATile {
 
     //Függvények
     public void accept(Visitor v, Directions d) {
-        tabulate.in();
-
         //mint minden accept meghívja a visitor pushTo metódusát önmagát átadva
         System.out.println(this.toString() + ".accept(" + v + "," + d + ")");
         v.pushTo(this, d);
-
-        tabulate.out();
     }
 
     public void setTrap(Trap t){
@@ -28,23 +23,16 @@ public class Switch extends ATile {
     }
 
     public void switchIt(Box b) {
-        tabulate.in();
-
         //ha box kerül a kapcsolóra vált - a trap kinyílik
         System.out.println(this.toString() + ".switchIt(" + b + ")");
         if (trap != null){
             trap.setOpened(true);
         } else {
-            tabulate.tabulate();
             System.out.println("Nem tartozik csapda a kapcsolohoz.");
         }
-
-        tabulate.out();
     }
 
     public void switchIt(Worker w){
-        tabulate.in();
-
         //ha worker kerül a kapcsolóra - a trap inaktív lesz
         System.out.println(this.toString() + ".switchIt(" + w + ")");
         if (trap != null){
@@ -52,8 +40,6 @@ public class Switch extends ATile {
         } else {
             System.out.println("Nem tartozik csapda a kapcsolohoz.");
         }
-
-        tabulate.out();
     }
 
     //objektum kiíráshoz
--- src/Trap.java
+++ src/Trap.java
@@ -1,12 +1,10 @@
 public class Trap extends ATile {
     //Attribútumok
-    //majd később szükség lesz rá, de jelenleg nem tárolunk állpotot ehhez
-    //private boolean opened;
+    private boolean opened;
 
     //név kiírására szolgáló számlálók
     static int instanceCounter = 0;
     int counter = 0;
-    //Tabulate tabulate = new Tabulate();
 
     public Trap() {
         instanceCounter++;
@@ -15,28 +13,20 @@ public class Trap extends ATile {
 
     //Függvények
     public void accept(Visitor v, Directions d) {
-        tabulate.in();
-
         //mint minden accept meghívja a visitor pushTo metódusát önmagát átadva
         System.out.println(this.toString() + ".accept(" + v + "," + d + ")");
         v.pushTo(this, d);
-
-        tabulate.out();
     }
 
     //ha visitor kerül, akkor attól függően, hogy milyen visitor,
     // kinyílik vagy becsukódik a csapda
     public void setOpened(Boolean bool) {
-        tabulate.in();
-
         System.out.println(this.toString() + ".setOpened(" + bool + ")");
-        //ez is a továbbiakban fog kelleni
-        //opened = bool
+
+        opened = bool;
         if(bool && visitor != null){
             visitor.die();
         }
-
-        tabulate.out();
     }
 
     //objektum kiíráshoz
