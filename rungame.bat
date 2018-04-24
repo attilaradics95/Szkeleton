a577afed8594e42a08fa505463c58cef3f96b48e
Rozina Zarka <rozi@zarka.hu>
2018-04-24 14:38:27 +0200

    rungame.bat föl

--- rungame.bat
+++ rungame.bat
@@ -1,9 +1,10 @@
 @echo off
 title Let's start running...
-set path =C:/mingw/bin;%path%
+set path =C:/MinGW/bin;%path%
 cd Compare
 g++ main.cpp -o Main
 cd ..
+javac -encoding cp1250 xyz.java
 cd src
 javac Game.java
 java Game
