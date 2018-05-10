b7b9c83f3191228b09f606be6e090f0391894c59
Radics Attila <attilaradics95@gmail.com>
2018-05-10 11:09:09 +0200

    -ActiveSwitchView eltávolítása

--- src/Switch.java
+++ src/Switch.java
@@ -48,7 +48,6 @@ public class Switch extends ATile {
     public void switchIt(Worker w){
         if (trap != null){
             trap.setOpened(false);
-            this.view = new ActiveSwitchView();
         } else {
             System.out.println("Nem tartozik csapda a kapcsolohoz.");
         }
