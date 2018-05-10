e78cd26d35a81aadd6d78b003bbca47657c2717b
Sándor Bálint <balint245@gmail.com>
2018-05-10 10:47:18 +0200

    MouseHandler Click függvény modosítás, GamePanel modosítás

--- src/MouseHandler.java
+++ src/MouseHandler.java
@@ -5,7 +5,9 @@ public class MouseHandler {
 
     private GameWindow gameWindow;
 
-    public void Click(MouseEvent e) {
+
+    //megkapja a GamePanel-től a tiles tömböt
+    public void Click(MouseEvent e, ATile[][] tiles) {
         //kattintas x koordinataja
         int x = e.getX();
         //kattintas y koordinataja
@@ -15,13 +17,12 @@ public class MouseHandler {
         int fieldX = x/60 + 1;
         int fieldY = y/60 + 1;
 
-        Worker selectedWorker = new Worker();
 
-        ATile[][] tiles = gameWindow.game.getTiles();
         if(tiles[fieldX][fieldY].getVisitor() != null) {
             JOptionPane.showMessageDialog(null, "Ez egy munkás!");
         } else {
             JOptionPane.showMessageDialog(null, "Ez nem munkás!");
         }
+        //JOptionPane.showMessageDialog(null, "Ez egy munkás!");
     }
 }
