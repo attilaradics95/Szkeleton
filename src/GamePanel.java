771c8186c4612b440b8ed94775984cbb9b225e01
Radics Attila <attilaradics95@gmail.com>
2018-05-13 12:20:47 +0200

    -keyPressed függvény módosítása

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -77,7 +77,25 @@ public class GamePanel extends JPanel implements MouseListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        switch(e.getKeyCode())
+        {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_UP:
+                //fel
+                break;
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_LEFT:
+                //bal
+                break;
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_DOWN:
+                //le
+                break;
+            case KeyEvent.VK_D:
+            case KeyEvent.VK_RIGHT:
+                //jobb
+                break;
+        }
     }
 
     @Override
