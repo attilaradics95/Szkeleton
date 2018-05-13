2f5bccb9dfd60eff4238cc9c37e9fc888904e5af
Radics Attila <attilaradics95@gmail.com>
2018-05-13 12:51:03 +0200

    -Billentyűkezelés dummy

--- src/MyListener.java
+++ src/MyListener.java
@@ -2,40 +2,39 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class MyListener implements KeyListener {
-    Controller controller;
+    Controller controller = Controller.getInstance();
 
     @Override
     public void keyTyped(KeyEvent e) {
-        System.out.println("KeyTyped");
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("KeyPressed");
 
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
             case KeyEvent.VK_UP:
                 System.out.println("fel");
-                //controller.moveWorker(Directions.NORTH);
+                controller.moveWorker(Directions.NORTH);
                 break;
 
             case KeyEvent.VK_A:
             case KeyEvent.VK_LEFT:
-
-                //controller.moveWorker(Directions.WEST);
+                System.out.println("bal");
+                controller.moveWorker(Directions.WEST);
                 break;
 
             case KeyEvent.VK_S:
             case KeyEvent.VK_DOWN:
-
-                //controller.moveWorker(Directions.EAST);
+                System.out.println("le");
+                controller.moveWorker(Directions.EAST);
                 break;
 
             case KeyEvent.VK_D:
             case KeyEvent.VK_RIGHT:
-
-                //controller.moveWorker(Directions.SOUTH);
+                System.out.println("jobb");
+                controller.moveWorker(Directions.SOUTH);
                 break;
 
         }
@@ -43,7 +42,6 @@ public class MyListener implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("KeyReleased");
 
     }
 
