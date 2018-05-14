8ddf2669ed380f63f62b9d651bb6b3b83a13078e
Rozina Zarka <rozi@zarka.hu>
2018-05-14 10:29:48 +0200

    WorkerView, Worker & Visitor kommentelve

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,25 +1,49 @@
+/**
+ * A worker és box osztályok absztrakt őse, a Visitor minta megvalósítása.
+ */
 public abstract class Visitor {
     protected int id;
     protected int force;
     protected ATile currentTile;
     protected ElementView view;
 
-
+    /**
+     * Visszaadja a visitor azonosítóját
+     * @return a visitor azonosítója
+     */
     public int getId() { return this.id; }
 
+    /**
+     * Visszaadja melyik mezőn áll éppen a visitor.
+     * @return a visitor tartózkodási helye
+     */
     public ATile getCurrentTile() { return this.currentTile; }
 
+    /**
+     * Beállítja a visitor erejét
+     * @param force a visitor ereje
+     */
     public void setForce(int force) {this.force = force; }
 
+    /**
+     * Visszaadja visitor erejét
+     * @return  a visitor ereje
+     */
     public int getForce() {return this.force; }
 
-    //setCurrentTile beállítja az aktuális Visitornak, hogy melyik mezőn áll
+    /**
+     * Beállítja az aktuális Visitornak, hogy melyik mezőn áll
+     * @param currentTile az aktuális mező
+     */
     public void setCurrentTile(ATile currentTile){
         this.currentTile = currentTile;
     }
 
-    //függvények, leszármazottakban implementálva
+    //
 
+    /**
+     * A függvények a leszármazottakban vannak implementálva
+     */
     abstract public void pushTo(Tile next, Directions d, int force);
     abstract public void pushTo(Switch next, Directions d, int force);
     abstract public void pushTo(Honey next, Directions d, int force);
